7b2eb040cffe0b1fcc6b29e92aa430d46ccd854b
may1029 <mymy.a1029@gmail.com>
2017-02-03 00:28:43 +0900

    move rename migration

--- db/migrate/20170202110806_rename_column_favorite.rb
+++
@@ -1,6 +0,0 @@
-class RenameColumnFavorite < ActiveRecord::Migration
-  def change
-    rename_column :favorites, :liked_id, :user_id
-    rename_column :favorites, :liking_id, :micropost_id
-  end
-end
